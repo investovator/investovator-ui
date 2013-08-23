446181963f6ccb9276de2c4c50112fd453633d80
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-08-23 12:23:42 +0530

    trying to add a basic chart using JASA data

--- src/main/java/org/investovator/MyVaadinUI.java
+++ src/main/java/org/investovator/MyVaadinUI.java
@@ -20,7 +20,11 @@ import com.vaadin.ui.Button.ClickEvent;
 //import org.dussan.vaadin.dcharts.options.Highlighter;
 //import org.dussan.vaadin.dcharts.options.Options;
 //import org.dussan.vaadin.dcharts.options.SeriesDefaults;
+import net.sourceforge.jabm.report.CombiSeriesReportVariables;
+import net.sourceforge.jabm.report.SimEventReport;
+import net.sourceforge.jabm.spring.BeanFactorySingleton;
 import org.investovator.utils.MainClassRunner;
+import org.springframework.beans.factory.BeanFactory;
 
 @Theme("mytheme")
 @SuppressWarnings("serial")
@@ -49,12 +53,93 @@ public class MyVaadinUI extends UI
         layout.addComponent(getChart());
 
 
+        Button button1 = new Button("drawGraph");
+        button1.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                 layout.addComponent(getBasicChart());
+
+            }
+        });
+        layout.addComponent(button1);
+
 
     }
 
     @Override
     public String getDescription() {
         return "Bart with negative stack";
+    }
+
+    protected Component getBasicChart(){
+        Chart chart = new Chart();
+        chart.setHeight("450px");
+        chart.setWidth("100%");
+
+        Configuration configuration = new Configuration();
+        configuration.getChart().setType(ChartType.LINE);
+        configuration.getChart().setMarginRight(130);
+        configuration.getChart().setMarginBottom(25);
+
+        configuration.getTitle().setText("Monthly Average Temperature");
+        configuration.getSubTitle().setText("Source: WorldClimate.com");
+
+        configuration.getxAxis().setCategories("Jan", "Feb", "Mar", "Apr",
+                "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+        Axis yAxis = configuration.getyAxis();
+        yAxis.setMin(-5d);
+        yAxis.setTitle(new Title("Temperature (°C)"));
+        yAxis.getTitle().setVerticalAlign(VerticalAlign.HIGH);
+
+        configuration
+                .getTooltip()
+                .setFormatter(
+                        "''+ this.series.name +''+this.x +': '+ this.y +'°C'");
+
+        PlotOptionsLine plotOptions = new PlotOptionsLine();
+        plotOptions.setDataLabels(new Labels(true));
+        configuration.setPlotOptions(plotOptions);
+
+        Legend legend = configuration.getLegend();
+        legend.setLayout(LayoutDirection.VERTICAL);
+        legend.setHorizontalAlign(HorizontalAlign.RIGHT);
+        legend.setVerticalAlign(VerticalAlign.TOP);
+        legend.setX(-10d);
+        legend.setY(100d);
+        legend.setBorderWidth(0);
+
+        ListSeries ls = new ListSeries();
+        ls.setName("Tokyo");
+        //ls.getPlotOptions().setPointStart(1959);
+
+        CombiSeriesReportVariables report = (CombiSeriesReportVariables)BeanFactorySingleton.getBean("priceTimeSeriesIBM");
+        for(int i=0;i<report.size(0);i++){
+//            System.out.println(report.getX(0,i).toString());
+//            layout.addComponent(new Label(report.getY(0,i).toString()));
+            ls.setData(report.getY(0,i));
+        }
+
+//        ls.setData(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3,
+//                13.9, 9.6);
+        configuration.addSeries(ls);
+//        ls = new ListSeries();
+//        ls.setName("New York");
+//        ls.setData(-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1,
+//                8.6, 2.5);
+//        configuration.addSeries(ls);
+//        ls = new ListSeries();
+//        ls.setName("Berlin");
+//        ls.setData(-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9,
+//                1.0);
+//        configuration.addSeries(ls);
+//        ls = new ListSeries();
+//        ls.setName("London");
+//        ls.setData(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6,
+//                4.8);
+//        configuration.addSeries(ls);
+
+        chart.drawChart(configuration);
+        return chart;
     }
 
 //    @Override
