d838d01c333e4507195de2b853a734afea1bce6d
hasalasurasinghe <hasala48@gmail.com>
2013-10-04 22:22:16 +0530

    AgentGamingView.java added, DataPlaybackView.java added

---
+++ src/main/java/org/investovator/Authentication/DataPlaybackView.java
@@ -0,0 +1,35 @@
+package org.investovator.Authentication;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hasala
+ * Date: 10/4/13
+ * Time: 5:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@SuppressWarnings("serial")
+public class DataPlaybackView extends VerticalLayout implements View {
+    Authenticator authenticator;
+
+    public DataPlaybackView(){
+        authenticator = Authenticator.getInstance();
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+        // TODO Auto-generated method stub
+        if(!authenticator.isLoggedIn()){
+            getUI().getNavigator().navigateTo("");
+        }
+        else{
+            Notification.show("Welcome to Data Playback Engine");
+        }
+    }
+
+}
+
