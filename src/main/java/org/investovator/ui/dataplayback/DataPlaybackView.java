af84a44da6d25e13508133943fa954c55f2fc02d
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-10-15 21:14:48 +0530

    Chart updates by obtaining data from the database (OHLC game)

--- src/main/java/org/investovator/ui/dataplayback/DataPlaybackView.java
+++ src/main/java/org/investovator/ui/dataplayback/DataPlaybackView.java
@@ -2,9 +2,8 @@ package org.investovator.ui.dataplayback;
 
 import com.vaadin.addon.charts.Chart;
 import com.vaadin.addon.charts.model.*;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import org.investovator.dataPlayBackEngine.DataPlayer;
 import org.investovator.ui.utils.dashboard.BasicDashboard;
 
 import java.util.LinkedHashMap;
@@ -16,6 +15,11 @@ import java.util.LinkedHashMap;
 
 @SuppressWarnings("serial")
 public class DataPlaybackView extends BasicDashboard {
+
+    DataPlayer player;
+    //used for counting data iteration number
+    int a=0;
+
 
 
     public DataPlaybackView() {
@@ -30,14 +34,7 @@ public class DataPlaybackView extends BasicDashboard {
     public LinkedHashMap<String, Panel> getMenuItems() {
         LinkedHashMap<String,Panel> map=new LinkedHashMap<String, Panel>();
 
-
-
-        VerticalLayout panelContent = new VerticalLayout();
-        panelContent.addComponent(buildMainChart());
-
-        Panel panel = new Panel();
-        panel.setContent(panelContent);
-        map.put("main view", panel);
+        map.put("main view", buildMainPanel());
 
 
 
@@ -55,6 +52,88 @@ public class DataPlaybackView extends BasicDashboard {
          */
 
         return map;
+    }
+
+
+    private Panel buildMainPanel(){
+        Panel panel = new Panel();
+
+        //add the main graph
+        VerticalLayout panelContent = new VerticalLayout();
+
+        HorizontalLayout topBar=new HorizontalLayout();
+        HorizontalLayout topButtonContainer=new HorizontalLayout();
+        topButtonContainer.setStyleName("sidebar");
+        topBar.addComponent(topButtonContainer);
+        //to set the alignment of the buttons
+        topBar.setComponentAlignment(topButtonContainer,Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setSizeFull();
+
+        panelContent.addComponent(topBar);
+        final Chart mainChart=buildMainChart();
+        panelContent.addComponent(mainChart);
+
+        //create the buttons
+        Button addGameButton=new Button("New Game");
+        Button playGameButton=new Button("Play Game");
+        Button pauseGameButton=new Button("Pause Game");
+        Button stopGameButton=new Button("Stop Game");
+        topButtonContainer.addComponent(addGameButton);
+        topButtonContainer.addComponent(playGameButton);
+        topButtonContainer.addComponent(pauseGameButton);
+        topButtonContainer.addComponent(stopGameButton);
+
+        //set alignments of the buttons
+        topButtonContainer.setComponentAlignment(addGameButton,Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(playGameButton,Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(pauseGameButton,Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(stopGameButton,Alignment.MIDDLE_RIGHT);
+//        topButtonContainer.setSpacing(false);
+
+        //add the action listeners for buttons
+        addGameButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                //TODO- fix this static value
+                String[] stocks=new String[2];
+                stocks[0]="GOOG";
+                stocks[1]="APPL";
+                player=new DataPlayer(stocks);
+            }
+        });
+        playGameButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+//                Notification.show(Float.toString(player.getOHLCPrice("Goog","2012-10-3-19-45-3"+Integer.toString(a))));
+                ListSeries series=(ListSeries)mainChart.getConfiguration().getSeries().get(0);
+                series.addData(player.getOHLCPrice("Goog","2012-10-3-19-45-3"+Integer.toString(a)));
+                a++;
+            }
+        });
+
+
+
+
+
+        Button nextDayB=new Button("Next day");
+        nextDayB.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                //Notification.show(Float.toString(player.getOHLCPrice("Goog","2012-10-3-19-45-3"+Integer.toString(a))));
+                ListSeries series=(ListSeries)mainChart.getConfiguration().getSeries().get(0);
+                series.addData(player.getOHLCPrice("Goog","2012-10-3-19-45-3"+Integer.toString(a)));
+                a++;
+            }
+        });
+
+
+        panelContent.addComponent(nextDayB);
+//        panelContent.addComponent(new Button("dd"));
+        //
+
+        panel.setContent(panelContent);
+
+        return  panel;
     }
 
     private Chart buildMainChart(){
@@ -78,26 +157,29 @@ public class DataPlaybackView extends BasicDashboard {
         plotOptions.setEnableMouseTracking(false);
         configuration.setPlotOptions(plotOptions);
 
-        configuration.getxAxis().setCategories("Jan", "Feb", "Mar", "Apr",
-                "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+//        configuration.getxAxis().setCategories("Jan", "Feb", "Mar", "Apr",
+//                "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 
         ListSeries ls = new ListSeries();
-        ls.setName("Short");
-        ls.setData(29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4,
-                194.1, 95.6, 54.4);
-        configuration.addSeries(ls);
-        ls = new ListSeries();
-        ls.setName("Long named series");
-        Number[] data = new Number[] { 129.9, 171.5, 106.4, 129.2, 144.0,
-                176.0, 135.6, 148.5, 216.4, 194.1, 195.6, 154.4 };
-        for (int i = 0; i < data.length / 2; i++) {
-            Number number = data[i];
-            data[i] = data[data.length - i - 1];
-            data[data.length - i - 1] = number;
-        }
+//        ls.setName("Short");
+//        ls.setData(29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4,
+//                194.1, 95.6, 54.4);
+//        configuration.addSeries(ls);
+//        ls = new ListSeries();
+        ls.setName("GOOG");
+//        Number[] data = new Number[] { 129.9, 171.5, 106.4, 129.2, 144.0,
+//                176.0, 135.6, 148.5, 216.4, 194.1, 195.6, 154.4 };
+//        for (int i = 0; i < data.length / 2; i++) {
+//            Number number = data[i];
+//            data[i] = data[data.length - i - 1];
+//            data[data.length - i - 1] = number;
+//        }
 
-        ls.setData(data);
+//        ls.setData(data);
         configuration.addSeries(ls);
+
+        //disable trademark
+        chart.getConfiguration().disableCredits();
 
         chart.drawChart(configuration);
         return chart;
--- src/main/java/org/investovator/ui/dataplayback/DataPlaybackViewBackup.java
+++ src/main/java/org/investovator/ui/dataplayback/DataPlaybackViewBackup.java
@@ -1,272 +1,269 @@
-package org.investovator.ui.dataplayback;
-
-import com.vaadin.addon.charts.Chart;
-import com.vaadin.addon.charts.ChartClickEvent;
-import com.vaadin.addon.charts.ChartClickListener;
-import com.vaadin.addon.charts.ChartOptions;
-import com.vaadin.addon.charts.model.*;
-import com.vaadin.addon.charts.model.style.SolidColor;
-import com.vaadin.addon.charts.model.style.Style;
-import com.vaadin.addon.charts.themes.GrayTheme;
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.ui.*;
-import org.investovator.core.data.types.HistoryOrderData;
-import org.investovator.core.excelimporter.HistoryData;
-import org.investovator.dataPlayBackEngine.DataPlayer;
-import org.investovator.ui.GlobalView;
-import org.investovator.ui.authentication.Authenticator;
-
-import java.util.Observable;
-import java.util.Observer;
-
-/**
- * Created with IntelliJ IDEA.
- * User: ishan
- * Date: 10/4/13
- * Time: 5:34 PM
- * To change this template use File | Settings | File Templates.
- */
-@SuppressWarnings("serial")
-public class DataPlaybackViewBackup extends GlobalView implements Observer {
-    Authenticator authenticator;
-    DataPlayer player;
-    Label entry;
-    int i = 0;
-
-    private CheckBox useCustomStyles;
-
-    public DataPlaybackViewBackup() {
-
-    }
-
-    public void setupUI(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
-        Notification.show("Welcome to Data Playback Engine");
-
-        authenticator = Authenticator.getInstance();
-        entry = new Label();
-        player = new DataPlayer("GOOG");
-        player.setObserver(this);
-
-        if (!authenticator.isLoggedIn()) {
-            getUI().getNavigator().navigateTo("");
-        } else {
-
-            //related to ticker data playback
-
-            Notification.show("Welcome to Data Playback Engine");
-            addComponent(entry);
-            addComponent(new Label("------------------------------------------------"));
-
-            Button startBut = new Button("play");
-            startBut.addClickListener(new Button.ClickListener() {
-                @Override
-                public void buttonClick(Button.ClickEvent clickEvent) {
-                    player.runPlayback(1);
-                }
-            });
-
-            Button stopBut = new Button("stop");
-            stopBut.addClickListener(new Button.ClickListener() {
-                @Override
-                public void buttonClick(Button.ClickEvent clickEvent) {
-                    player.stopPlayback();
-                }
-            });
-            addComponent(startBut);
-            addComponent(stopBut);
-
-//            end of related to ticker data playback
-
-
-            //boxplot
-            final Chart chart = new Chart();
-
-            ChartOptions.get().setTheme(new GrayTheme());
-
-            chart.getConfiguration().setTitle("Box Plot Example");
-
-            Legend legend = new Legend();
-            legend.setEnabled(false);
-            chart.getConfiguration().setLegend(legend);
-
-            XAxis xaxis = chart.getConfiguration().getxAxis();
-            xaxis.setTitle("Experiment No.");
-            //xaxis.setCategories("1", "2", "3", "4", "5");
-            xaxis.setTickInterval(1);
-            //chart.getConfiguration().getM
-
-            YAxis yAxis = chart.getConfiguration().getyAxis();
-
-            yAxis.setTitle("Observations");
-            PlotLine plotLine = new PlotLine();
-            plotLine.setColor(new SolidColor("red"));
-            plotLine.setValue(932);
-            plotLine.setWidth(1);
-            PlotBandLabel label = new PlotBandLabel("Theoretical mean: 932");
-            label.setAlign(HorizontalAlign.CENTER);
-            Style style = new Style();
-            style.setColor(new SolidColor("gray"));
-            label.setStyle(style);
-            plotLine.setLabel(label);
-            yAxis.setPlotLines(plotLine);
-
-            //stops animation on graph redraw
-            PlotOptionsBoxPlot plotOptions = new PlotOptionsBoxPlot();
-            plotOptions.setAnimation(false);
-            chart.getConfiguration().setPlotOptions(plotOptions);
-
-
-            final DataSeries observations = new DataSeries();
-            final DataSeries ss=new DataSeries();
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
-
-            observations.setName("Observations");
-
-            // Add PlotBoxItems contain all fields relevant for plot box chart
-            observations.add(new BoxPlotItem(760, 801, 848, 895, 965));
-
-            // Example with no arg constructor
-            BoxPlotItem plotBoxItem = new BoxPlotItem();
-            plotBoxItem.setLow(733);
-            plotBoxItem.setLowerQuartile(853);
-            plotBoxItem.setMedian(939);
-            plotBoxItem.setUpperQuartile(980);
-            plotBoxItem.setHigh(1080);
-            observations.add(plotBoxItem);
-
-            observations.add(new BoxPlotItem(714, 762, 817, 870, 918));
-            observations.add(new BoxPlotItem(724, 802, 806, 871, 950));
-            observations.add(new BoxPlotItem(724, 802, 806, 871, 950));
-            observations.add(new BoxPlotItem(834, 836, 864, 882, 910));
-            observations.setPlotOptions(getPlotBoxOptions());
-            chart.getConfiguration().addSeries(observations);
-            chart.getConfiguration().addSeries(ss);
-
-
-            chart.drawChart();
-
-            //threaded
-
-            Thread randomDataGenerator = new Thread() {
-                @Override
-                public void run() {
-                    while (true) {
-                        try {
-                            sleep(1000);
-                            if (chart.isConnectorEnabled()) {
-                                getSession().lock();
-                                try {
-                                    observations.add(new BoxPlotItem(834, 836, 864, 882, 910));
-
-                                    HistoryData d = player.getOHLCPrice("Goog", "2012-10-3-19-45-33");
-
-                                    observations.add(new BoxPlotItem(d.getLowPrice(), d.getLowPrice() + 25, d.getLowPrice() + 50, d.getLowPrice() + 75, d.getHighPrice()));
-                                    ss.add(new BoxPlotItem(d.getLowPrice(), d.getLowPrice() + 25, d.getLowPrice() + 50, d.getLowPrice() + 75, d.getHighPrice()));
-                                    chart.drawChart();
-                                    System.out.println("called");
-                                } finally {
-                                    getSession().unlock();
-                                }
-                            } else {
-                                break;
-                            }
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            };
-
-
-            randomDataGenerator.start();
-
-            ///threaded end
-
-
-            //end of boxplot
-
-            //OHLC data playback related
-            Button ohlcStartBut = new Button("play OHLC");
-            ohlcStartBut.addClickListener(new Button.ClickListener() {
-                @Override
-                public void buttonClick(Button.ClickEvent clickEvent) {
-                    final HistoryData d = player.getOHLCPrice("Goog", "2012-10-3-19-45-33");
-
-                    observations.add(new BoxPlotItem(884, 886, 914, 932, 960));
-                    chart.drawChart();
-                }
-            });
-
-            addComponent(ohlcStartBut);
-
-
-            chart.addChartClickListener(new ChartClickListener() {
-                @Override
-                public void onClick(ChartClickEvent event) {
-                    observations.add(new BoxPlotItem(884, 886, 914, 932, 960));
-                    chart.drawChart();
-                    System.out.println("done");
-
-                }
-            });
-
-            addComponent(chart);
-
-    }
-
-
-
-    }
-
-    private PlotOptionsBoxPlot getPlotBoxOptions() {
-        PlotOptionsBoxPlot options = new PlotOptionsBoxPlot();
-
-        if (false) {
-            // optional styling
-            options.setMedianColor(new SolidColor("cyan"));
-            options.setMedianWidth(1);
-
-            options.setStemColor(new SolidColor("green"));
-            options.setStemDashStyle(DashStyle.SHORTDOT);
-            options.setStemWidth(4);
-
-            options.setWhiskerColor(new SolidColor("magenta"));
-            options.setWhiskerLength(70);
-            options.setWhiskerWidth(5);
-        }
-
-        return options;
-    }
-
-
-    @Override
-    public void update(Observable o, Object arg) {
-        final HistoryOrderData d = (HistoryOrderData) arg;
-
-        getUI().access(new Runnable() {
-            @Override
-            public void run() {
-                entry.setValue(d.getDate() + "-" + d.getStockId() + "-" + i);
-                addComponent(new Label(d.getDate() + "-" + d.getStockId() + "-" + i));
-                i++;
-
-
-            }
-        });
-
-
-    }
-
-
-}
-
+//package org.investovator.ui.dataplayback;
+//
+//import com.vaadin.addon.charts.Chart;
+//import com.vaadin.addon.charts.ChartClickEvent;
+//import com.vaadin.addon.charts.ChartClickListener;
+//import com.vaadin.addon.charts.ChartOptions;
+//import com.vaadin.addon.charts.model.*;
+//import com.vaadin.addon.charts.model.style.SolidColor;
+//import com.vaadin.addon.charts.model.style.Style;
+//import com.vaadin.addon.charts.themes.GrayTheme;
+//import com.vaadin.navigator.View;
+//import com.vaadin.navigator.ViewChangeListener;
+//import com.vaadin.ui.*;
+//import org.investovator.core.data.types.HistoryOrderData;
+//import org.investovator.core.excelimporter.HistoryData;
+//import org.investovator.dataPlayBackEngine.DataPlayer;
+//import org.investovator.ui.GlobalView;
+//import org.investovator.ui.authentication.Authenticator;
+//
+//import java.util.Observable;
+//import java.util.Observer;
+//
+///**
+// * @author: ishan
+// * @version: ${Revision}
+// */
+//@SuppressWarnings("serial")
+//public class DataPlaybackViewBackup extends GlobalView implements Observer {
+//    Authenticator authenticator;
+//    DataPlayer player;
+//    Label entry;
+//    int i = 0;
+//
+//    private CheckBox useCustomStyles;
+//
+//    public DataPlaybackViewBackup() {
+//
+//    }
+//
+//    public void setupUI(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+//        Notification.show("Welcome to Data Playback Engine");
+//
+//        authenticator = Authenticator.getInstance();
+//        entry = new Label();
+//        player = new DataPlayer("GOOG");
+//        player.setObserver(this);
+//
+//        if (!authenticator.isLoggedIn()) {
+//            getUI().getNavigator().navigateTo("");
+//        } else {
+//
+//            //related to ticker data playback
+//
+//            Notification.show("Welcome to Data Playback Engine");
+//            addComponent(entry);
+//            addComponent(new Label("------------------------------------------------"));
+//
+//            Button startBut = new Button("play");
+//            startBut.addClickListener(new Button.ClickListener() {
+//                @Override
+//                public void buttonClick(Button.ClickEvent clickEvent) {
+//                    player.runPlayback(1);
+//                }
+//            });
+//
+//            Button stopBut = new Button("stop");
+//            stopBut.addClickListener(new Button.ClickListener() {
+//                @Override
+//                public void buttonClick(Button.ClickEvent clickEvent) {
+//                    player.stopPlayback();
+//                }
+//            });
+//            addComponent(startBut);
+//            addComponent(stopBut);
+//
+////            end of related to ticker data playback
+//
+//
+//            //boxplot
+//            final Chart chart = new Chart();
+//
+//            ChartOptions.get().setTheme(new GrayTheme());
+//
+//            chart.getConfiguration().setTitle("Box Plot Example");
+//
+//            Legend legend = new Legend();
+//            legend.setEnabled(false);
+//            chart.getConfiguration().setLegend(legend);
+//
+//            XAxis xaxis = chart.getConfiguration().getxAxis();
+//            xaxis.setTitle("Experiment No.");
+//            //xaxis.setCategories("1", "2", "3", "4", "5");
+//            xaxis.setTickInterval(1);
+//            //chart.getConfiguration().getM
+//
+//            YAxis yAxis = chart.getConfiguration().getyAxis();
+//
+//            yAxis.setTitle("Observations");
+//            PlotLine plotLine = new PlotLine();
+//            plotLine.setColor(new SolidColor("red"));
+//            plotLine.setValue(932);
+//            plotLine.setWidth(1);
+//            PlotBandLabel label = new PlotBandLabel("Theoretical mean: 932");
+//            label.setAlign(HorizontalAlign.CENTER);
+//            Style style = new Style();
+//            style.setColor(new SolidColor("gray"));
+//            label.setStyle(style);
+//            plotLine.setLabel(label);
+//            yAxis.setPlotLines(plotLine);
+//
+//            //stops animation on graph redraw
+//            PlotOptionsBoxPlot plotOptions = new PlotOptionsBoxPlot();
+//            plotOptions.setAnimation(false);
+//            chart.getConfiguration().setPlotOptions(plotOptions);
+//
+//
+//            final DataSeries observations = new DataSeries();
+//            final DataSeries ss=new DataSeries();
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            ss.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//
+//            observations.setName("Observations");
+//
+//            // Add PlotBoxItems contain all fields relevant for plot box chart
+//            observations.add(new BoxPlotItem(760, 801, 848, 895, 965));
+//
+//            // Example with no arg constructor
+//            BoxPlotItem plotBoxItem = new BoxPlotItem();
+//            plotBoxItem.setLow(733);
+//            plotBoxItem.setLowerQuartile(853);
+//            plotBoxItem.setMedian(939);
+//            plotBoxItem.setUpperQuartile(980);
+//            plotBoxItem.setHigh(1080);
+//            observations.add(plotBoxItem);
+//
+//            observations.add(new BoxPlotItem(714, 762, 817, 870, 918));
+//            observations.add(new BoxPlotItem(724, 802, 806, 871, 950));
+//            observations.add(new BoxPlotItem(724, 802, 806, 871, 950));
+//            observations.add(new BoxPlotItem(834, 836, 864, 882, 910));
+//            observations.setPlotOptions(getPlotBoxOptions());
+//            chart.getConfiguration().addSeries(observations);
+//            chart.getConfiguration().addSeries(ss);
+//
+//
+//            chart.drawChart();
+//
+//            //threaded
+//
+//            Thread randomDataGenerator = new Thread() {
+//                @Override
+//                public void run() {
+//                    while (true) {
+//                        try {
+//                            sleep(1000);
+//                            if (chart.isConnectorEnabled()) {
+//                                getSession().lock();
+//                                try {
+//                                    observations.add(new BoxPlotItem(834, 836, 864, 882, 910));
+//
+//                                    HistoryData d = player.getOHLCPrice("Goog", "2012-10-3-19-45-33");
+//
+//                                    observations.add(new BoxPlotItem(d.getLowPrice(), d.getLowPrice() + 25, d.getLowPrice() + 50, d.getLowPrice() + 75, d.getHighPrice()));
+//                                    ss.add(new BoxPlotItem(d.getLowPrice(), d.getLowPrice() + 25, d.getLowPrice() + 50, d.getLowPrice() + 75, d.getHighPrice()));
+//                                    chart.drawChart();
+//                                    System.out.println("called");
+//                                } finally {
+//                                    getSession().unlock();
+//                                }
+//                            } else {
+//                                break;
+//                            }
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            };
+//
+//
+//            randomDataGenerator.start();
+//
+//            ///threaded end
+//
+//
+//            //end of boxplot
+//
+//            //OHLC data playback related
+//            Button ohlcStartBut = new Button("play OHLC");
+//            ohlcStartBut.addClickListener(new Button.ClickListener() {
+//                @Override
+//                public void buttonClick(Button.ClickEvent clickEvent) {
+//                    final HistoryData d = player.getOHLCPrice("Goog", "2012-10-3-19-45-33");
+//
+//                    observations.add(new BoxPlotItem(884, 886, 914, 932, 960));
+//                    chart.drawChart();
+//                }
+//            });
+//
+//            addComponent(ohlcStartBut);
+//
+//
+//            chart.addChartClickListener(new ChartClickListener() {
+//                @Override
+//                public void onClick(ChartClickEvent event) {
+//                    observations.add(new BoxPlotItem(884, 886, 914, 932, 960));
+//                    chart.drawChart();
+//                    System.out.println("done");
+//
+//                }
+//            });
+//
+//            addComponent(chart);
+//
+//    }
+//
+//
+//
+//    }
+//
+//    private PlotOptionsBoxPlot getPlotBoxOptions() {
+//        PlotOptionsBoxPlot options = new PlotOptionsBoxPlot();
+//
+//        if (false) {
+//            // optional styling
+//            options.setMedianColor(new SolidColor("cyan"));
+//            options.setMedianWidth(1);
+//
+//            options.setStemColor(new SolidColor("green"));
+//            options.setStemDashStyle(DashStyle.SHORTDOT);
+//            options.setStemWidth(4);
+//
+//            options.setWhiskerColor(new SolidColor("magenta"));
+//            options.setWhiskerLength(70);
+//            options.setWhiskerWidth(5);
+//        }
+//
+//        return options;
+//    }
+//
+//
+//    @Override
+//    public void update(Observable o, Object arg) {
+//        final HistoryOrderData d = (HistoryOrderData) arg;
+//
+//        getUI().access(new Runnable() {
+//            @Override
+//            public void run() {
+//                entry.setValue(d.getDate() + "-" + d.getStockId() + "-" + i);
+//                addComponent(new Label(d.getDate() + "-" + d.getStockId() + "-" + i));
+//                i++;
+//
+//
+//            }
+//        });
+//
+//
+//    }
+//
+//
+//}
+//
