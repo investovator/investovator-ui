a5210a5fd7354ddf8e75357e63980cd296afea04
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-10-21 01:56:06 +0530

    Stock price table completed for Ticker data

--- src/main/java/org/investovator/ui/dataplayback/DataPlaybackMainView.java
+++ src/main/java/org/investovator/ui/dataplayback/DataPlaybackMainView.java
@@ -21,6 +21,8 @@ package org.investovator.ui.dataplayback;
 
 import com.vaadin.addon.charts.Chart;
 import com.vaadin.addon.charts.model.*;
+import com.vaadin.data.util.BeanContainer;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.*;
 import org.investovator.core.data.api.utils.TradingDataAttribute;
 import org.investovator.dataplaybackengine.OHLCDataPLayer;
@@ -29,6 +31,7 @@ import org.investovator.dataplaybackengine.events.EventManager;
 import org.investovator.dataplaybackengine.events.StockEvent;
 import org.investovator.dataplaybackengine.exceptions.GameAlreadyStartedException;
 import org.investovator.dataplaybackengine.exceptions.GameFinishedException;
+import org.investovator.ui.dataplayback.beans.StockNamePriceBean;
 import org.investovator.ui.dataplayback.util.DataPLaybackEngineGameTypes;
 import org.investovator.ui.dataplayback.util.DataPlaybackEngineStates;
 import org.investovator.ui.dataplayback.wizards.NewDataPlaybackGameWizard;
@@ -44,10 +47,10 @@ import java.util.Observer;
 public class DataPlaybackMainView extends Panel implements Observer {
 
     //decides the number of points shown in the ticker chart
-    private static int TICKER_CHART_LENGTH=10;
+    private static int TICKER_CHART_LENGTH = 10;
 
     //decides the number of points shown in the OHLC chart
-    private static int OHLC_CHART_LENGTH=10;
+    private static int OHLC_CHART_LENGTH = 10;
 
     OHLCDataPLayer ohlcPLayer;
     RealTimeDataPlayer realTimePlayer;
@@ -59,20 +62,21 @@ public class DataPlaybackMainView extends Panel implements Observer {
     Chart ohlcChart;
     Chart tickerChart;
 
+    Table stockPriceTable;
+
     //to store every component
     GridLayout content;
 
     public DataPlaybackMainView() {
         //set a link to this class
-        mySelf=this;
+        mySelf = this;
 
-        content = new GridLayout(3,3);
+        content = new GridLayout(3, 3);
         content.setSizeFull();
 
         ////for testing
-        DataPlaybackEngineStates.currentGameMode=DataPLaybackEngineGameTypes.OHLC_BASED;
+        DataPlaybackEngineStates.currentGameMode = DataPLaybackEngineGameTypes.OHLC_BASED;
         this.setUpGame(true);
-
 
 
         //
@@ -231,7 +235,7 @@ public class DataPlaybackMainView extends Panel implements Observer {
     }
 
 
-    private Chart buildOHLCChart(){
+    private Chart buildOHLCChart() {
         Chart chart = new Chart();
 //        chart.setHeight("350px");
 //        chart.setWidth("90%");
@@ -252,8 +256,8 @@ public class DataPlaybackMainView extends Panel implements Observer {
         plotOptions.setEnableMouseTracking(false);
         configuration.setPlotOptions(plotOptions);
 
-        if(DataPlaybackEngineStates.playingSymbols!=null){
-            for(String stock:DataPlaybackEngineStates.playingSymbols){
+        if (DataPlaybackEngineStates.playingSymbols != null) {
+            for (String stock : DataPlaybackEngineStates.playingSymbols) {
                 DataSeries ls = new DataSeries();
                 ls.setName(stock);
                 configuration.addSeries(ls);
@@ -269,7 +273,7 @@ public class DataPlaybackMainView extends Panel implements Observer {
         return chart;
     }
 
-    private Chart buildTickerChart(){
+    private Chart buildTickerChart() {
         tickerChart = new Chart();
 //        tickerChart.setHeight("350px");
 //        tickerChart.setWidth("250px");
@@ -291,17 +295,14 @@ public class DataPlaybackMainView extends Panel implements Observer {
         plotOptions.setEnableMouseTracking(false);
         configuration.setPlotOptions(plotOptions);
 
-        if(DataPlaybackEngineStates.playingSymbols!=null){
-            for(String stock:DataPlaybackEngineStates.playingSymbols){
+        if (DataPlaybackEngineStates.playingSymbols != null) {
+            for (String stock : DataPlaybackEngineStates.playingSymbols) {
                 DataSeries ls = new DataSeries();
                 ls.setName(stock);
                 configuration.addSeries(ls);
 
             }
         }
-
-
-
 
 
         tickerChart.drawChart(configuration);
@@ -313,7 +314,7 @@ public class DataPlaybackMainView extends Panel implements Observer {
         return tickerChart;
     }
 
-    private void startAddGameWizard(){
+    private void startAddGameWizard() {
         // Create a sub-window and set the content
         Window subWindow = new Window("Create New Game");
         VerticalLayout subContent = new VerticalLayout();
@@ -321,7 +322,7 @@ public class DataPlaybackMainView extends Panel implements Observer {
         subWindow.setContent(subContent);
 
         // Put some components in it
-        subContent.addComponent(new NewDataPlaybackGameWizard(subWindow,this));
+        subContent.addComponent(new NewDataPlaybackGameWizard(subWindow, this));
 
         // set window characteristics
         subWindow.center();
@@ -345,32 +346,34 @@ public class DataPlaybackMainView extends Panel implements Observer {
         if (DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.OHLC_BASED && !initialization) {
             //TODO - find a proper place to define attributes
             //define the attributes needed
-            TradingDataAttribute attributes[]=new TradingDataAttribute[2];
+            TradingDataAttribute attributes[] = new TradingDataAttribute[2];
 
             //just the closing price is enough for now
-            attributes[0]=TradingDataAttribute.DAY;
-            attributes[1]=TradingDataAttribute.PRICE;
+            attributes[0] = TradingDataAttribute.DAY;
+            attributes[1] = TradingDataAttribute.PRICE;
 
             try {
-                ohlcPLayer=new OHLCDataPLayer(DataPlaybackEngineStates.playingSymbols,attributes);
+                ohlcPLayer = new OHLCDataPLayer(DataPlaybackEngineStates.playingSymbols, attributes);
                 ohlcPLayer.setStartDate(DataPlaybackEngineStates.gameStartDate);
             } catch (ParseException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
         }
         //if the game type is ticker data based
-        else if(DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.TICKER_BASED && !initialization){
+        else if (DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.TICKER_BASED &&
+                !initialization) {
 
             //TODO - find a proper place to define attributes
             //define the attributes needed
-            TradingDataAttribute attributes[]=new TradingDataAttribute[3];
+            TradingDataAttribute attributes[] = new TradingDataAttribute[3];
 
             //just the closing price is enough for now
-            attributes[0]=TradingDataAttribute.DAY;
-            attributes[1]=TradingDataAttribute.PRICE;
-            attributes[2]=TradingDataAttribute.SHARES;
+            attributes[0] = TradingDataAttribute.DAY;
+            attributes[1] = TradingDataAttribute.PRICE;
+            attributes[2] = TradingDataAttribute.SHARES;
 
-            realTimePlayer=new RealTimeDataPlayer(DataPlaybackEngineStates.playingSymbols,DataPlaybackEngineStates.gameStartDate,attributes);
+            realTimePlayer = new RealTimeDataPlayer(DataPlaybackEngineStates.playingSymbols,
+                    DataPlaybackEngineStates.gameStartDate, attributes);
 
             //set myself as an observer
             realTimePlayer.setObserver(this);
@@ -379,8 +382,8 @@ public class DataPlaybackMainView extends Panel implements Observer {
         }
 
         //Top buttons
-        HorizontalLayout topBar=new HorizontalLayout();
-        HorizontalLayout topButtonContainer=new HorizontalLayout();
+        HorizontalLayout topBar = new HorizontalLayout();
+        HorizontalLayout topButtonContainer = new HorizontalLayout();
         topButtonContainer.setStyleName("sidebar");
         topBar.addComponent(topButtonContainer);
 //        topBar.setSizeFull();
@@ -392,20 +395,20 @@ public class DataPlaybackMainView extends Panel implements Observer {
         content.setComponentAlignment(topBar, Alignment.TOP_RIGHT);
 
         //create the buttons
-        Button addGameButton=new Button("New Game");
-        Button playGameButton=new Button("Play Game");
-        Button pauseGameButton=new Button("Pause Game");
-        Button stopGameButton=new Button("Stop Game");
+        Button addGameButton = new Button("New Game");
+        Button playGameButton = new Button("Play Game");
+        Button pauseGameButton = new Button("Pause Game");
+        Button stopGameButton = new Button("Stop Game");
         topButtonContainer.addComponent(addGameButton);
         topButtonContainer.addComponent(playGameButton);
         topButtonContainer.addComponent(pauseGameButton);
         topButtonContainer.addComponent(stopGameButton);
 
         //set alignments of the buttons
-        topButtonContainer.setComponentAlignment(addGameButton,Alignment.MIDDLE_RIGHT);
-        topButtonContainer.setComponentAlignment(playGameButton,Alignment.MIDDLE_RIGHT);
-        topButtonContainer.setComponentAlignment(pauseGameButton,Alignment.MIDDLE_RIGHT);
-        topButtonContainer.setComponentAlignment(stopGameButton,Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(addGameButton, Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(playGameButton, Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(pauseGameButton, Alignment.MIDDLE_RIGHT);
+        topButtonContainer.setComponentAlignment(stopGameButton, Alignment.MIDDLE_RIGHT);
 
         //add the action listeners for buttons
         addGameButton.addClickListener(new Button.ClickListener() {
@@ -421,11 +424,11 @@ public class DataPlaybackMainView extends Panel implements Observer {
             public void buttonClick(Button.ClickEvent clickEvent) {
 
                 //if an OHLC based game
-                if(DataPlaybackEngineStates.currentGameMode==DataPLaybackEngineGameTypes.OHLC_BASED){
+                if (DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.OHLC_BASED) {
                     //TODO - what if there were multiple serieses?
-                    DataSeries series=(DataSeries)ohlcChart.getConfiguration().getSeries().get(0);
+                    DataSeries series = (DataSeries) ohlcChart.getConfiguration().getSeries().get(0);
                     try {
-                        series.add(new DataSeriesItem(ohlcPLayer.getToday(),ohlcPLayer.startGame()[0].
+                        series.add(new DataSeriesItem(ohlcPLayer.getToday(), ohlcPLayer.startGame()[0].
                                 getData().get(TradingDataAttribute.PRICE)));
                     } catch (GameAlreadyStartedException e) {
                         e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
@@ -433,14 +436,13 @@ public class DataPlaybackMainView extends Panel implements Observer {
 
                 }
                 //if a ticker based game
-                else if(DataPlaybackEngineStates.currentGameMode==DataPLaybackEngineGameTypes.TICKER_BASED){
+                else if (DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.TICKER_BASED) {
                     //TODO - what if there were multiple serieses?
-                    DataSeries series=(DataSeries)tickerChart.getConfiguration().getSeries().get(0);
+                    DataSeries series = (DataSeries) tickerChart.getConfiguration().getSeries().get(0);
                     //TODO - how to set resolution?
                     realTimePlayer.startPlayback(1);
 
                 }
-
 
 
             }
@@ -455,32 +457,32 @@ public class DataPlaybackMainView extends Panel implements Observer {
 //        });
 
 
-
-
-        Button nextDayB=new Button("Next day");
+        Button nextDayB = new Button("Next day");
         nextDayB.addClickListener(new Button.ClickListener() {
             @Override
             public void buttonClick(Button.ClickEvent clickEvent) {
 
                 //get the events
                 try {
-                    StockEvent[] events= ohlcPLayer.playNextDay();
+                    StockEvent[] events = ohlcPLayer.playNextDay();
                     //iterate every event
-                    for(StockEvent event:events){
+                    for (StockEvent event : events) {
                         //iterate every series in the chart at the moment
-                        for(Series series:ohlcChart.getConfiguration().getSeries()){
-                            DataSeries dSeries=(DataSeries)series;
+                        for (Series series : ohlcChart.getConfiguration().getSeries()) {
+                            DataSeries dSeries = (DataSeries) series;
                             //if there's a match
-                            if(event.getStockId().equals(dSeries.getName())){
+                            if (event.getStockId().equals(dSeries.getName())) {
                                 if (ohlcChart.isConnectorEnabled()) {
                                     getSession().lock();
-                                    try{
+                                    try {
                                         if (dSeries.getData().size() > OHLC_CHART_LENGTH) {
 
-                                            dSeries.add(new DataSeriesItem(event.getTime(), event.getData().get(TradingDataAttribute.PRICE)), true, true);
+                                            dSeries.add(new DataSeriesItem(event.getTime(),
+                                                    event.getData().get(TradingDataAttribute.PRICE)), true, true);
 
                                         } else {
-                                            dSeries.add(new DataSeriesItem(event.getTime(), event.getData().get(TradingDataAttribute.PRICE)));
+                                            dSeries.add(new DataSeriesItem(event.getTime(),
+                                                    event.getData().get(TradingDataAttribute.PRICE)));
 
                                         }
                                         ohlcChart.setImmediate(true);
@@ -498,11 +500,9 @@ public class DataPlaybackMainView extends Panel implements Observer {
                     }
 
 
-
                 } catch (GameFinishedException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
-
 
 
 //
@@ -529,7 +529,6 @@ public class DataPlaybackMainView extends Panel implements Observer {
 //                }
 
 
-
             }
         });
 
@@ -537,49 +536,69 @@ public class DataPlaybackMainView extends Panel implements Observer {
         content.setComponentAlignment(nextDayB, Alignment.MIDDLE_CENTER);
 
         //Main chart
-        HorizontalLayout chartContainer=new HorizontalLayout();
-        chartContainer.setWidth(95,Unit.PERCENTAGE);
+        HorizontalLayout chartContainer = new HorizontalLayout();
+        chartContainer.setWidth(95, Unit.PERCENTAGE);
 
         //if the game type is OHLC
         if (DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.OHLC_BASED) {
-            ohlcChart= buildOHLCChart();
+            ohlcChart = buildOHLCChart();
             chartContainer.addComponent(ohlcChart);
-            chartContainer.setComponentAlignment(ohlcChart,Alignment.MIDDLE_CENTER);
+            chartContainer.setComponentAlignment(ohlcChart, Alignment.MIDDLE_CENTER);
         }
         //if the game type is ticker data based
-        else if(DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.TICKER_BASED){
-            tickerChart=buildTickerChart();
+        else if (DataPlaybackEngineStates.currentGameMode == DataPLaybackEngineGameTypes.TICKER_BASED) {
+            tickerChart = buildTickerChart();
             chartContainer.addComponent(tickerChart);
-            chartContainer.setComponentAlignment(tickerChart,Alignment.MIDDLE_CENTER);
+            chartContainer.setComponentAlignment(tickerChart, Alignment.MIDDLE_CENTER);
 
         }
         content.addComponent(chartContainer, 0, 1, 2, 1);
         content.setComponentAlignment(chartContainer, Alignment.MIDDLE_CENTER);
 
+        //Stock price table
+        stockPriceTable=(Table)setupStockPriceTable();
+        content.addComponent(stockPriceTable,0,2);
+        content.setComponentAlignment(stockPriceTable,Alignment.BOTTOM_LEFT);
+
+
 
         this.setContent(content);
 
 
+    }
 
+    public Component setupStockPriceTable(){
 
+        BeanContainer<String,StockNamePriceBean> beans =
+                new BeanContainer<String,StockNamePriceBean>(StockNamePriceBean.class);
+        beans.setBeanIdProperty("stockID");
+        //if the game is initialized
+        if(DataPlaybackEngineStates.playingSymbols!=null){
+            for(String stock:DataPlaybackEngineStates.playingSymbols){
+                beans.addBean(new StockNamePriceBean(stock,0));
+            }
+        }
+        Table table=new Table("Stock Prices",beans);
+
+        return table;
     }
 
 
     @Override
     public void update(Observable o, Object arg) {
         //if this is a stock price update
-        if(arg instanceof StockEvent){
-            final StockEvent event=(StockEvent) arg;
+        if (arg instanceof StockEvent) {
+            final StockEvent event = (StockEvent) arg;
 
             //iterate every series in the chart at the moment
-            for(Series series:tickerChart.getConfiguration().getSeries()){
-                DataSeries dSeries=(DataSeries)series;
+            for (Series series : tickerChart.getConfiguration().getSeries()) {
+                DataSeries dSeries = (DataSeries) series;
                 //if this series matches the stock events stock
-                if(dSeries.getName().equalsIgnoreCase(event.getStockId())){
+                if (dSeries.getName().equalsIgnoreCase(event.getStockId())) {
 
                     if (tickerChart.isConnectorEnabled()) {
                         getSession().lock();
-                        try{
+                        try {
                             if (dSeries.getData().size() > TICKER_CHART_LENGTH) {
 
                                 dSeries.add(new DataSeriesItem(event.getTime(), event.getData().get(TradingDataAttribute.PRICE)), true, true);
@@ -596,10 +615,27 @@ public class DataPlaybackMainView extends Panel implements Observer {
                     }
 
 
-
                 }
 
             }
+
+            //update the table
+            BeanContainer<String,StockNamePriceBean> beans = (BeanContainer<String,StockNamePriceBean>)
+                    stockPriceTable.getContainerDataSource();
+
+
+            if (stockPriceTable.isConnectorEnabled()) {
+                getSession().lock();
+                try {
+//                    beans.getItem(event.getStockId()).getBean().setPrice(event.getData().get(TradingDataAttribute.PRICE));
+                    beans.removeItem(event.getStockId());
+                    beans.addBean(new StockNamePriceBean(event.getStockId(),event.getData().get(TradingDataAttribute.PRICE)));
+                } finally {
+                    getSession().unlock();
+                }
+            }
+
+
 
 
 //            //TODO - only updates for GOOG stocks
@@ -626,10 +662,9 @@ public class DataPlaybackMainView extends Panel implements Observer {
 //            }
 
 
-
         }
         //if the game has stopped
-        else if(arg == EventManager.RealTimePlayerStates.GAME_OVER){
+        else if (arg == EventManager.RealTimePlayerStates.GAME_OVER) {
             //TODO - how to handle this?
         }
 
---
+++ src/main/java/org/investovator/ui/dataplayback/beans/StockNamePriceBean.java
@@ -0,0 +1,53 @@
+/*
+ * investovator, Stock Market Gaming Framework
+ *     Copyright (C) 2013  investovator
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package org.investovator.ui.dataplayback.beans;
+
+import java.io.Serializable;
+
+/**
+ * @author: ishan
+ * @version: ${Revision}
+ */
+public class StockNamePriceBean implements Serializable {
+
+    String stockID;
+    float price;
+
+    public StockNamePriceBean(String stockID, float price) {
+        this.stockID = stockID;
+        this.price = price;
+    }
+
+    public String getStockID() {
+        return stockID;
+    }
+
+    public void setStockID(String stockID) {
+        this.stockID = stockID;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
