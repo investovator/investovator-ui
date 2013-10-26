4f778c1572089a7379edc6a57a7abe7ed8dc3a8c
Amila Surendra <amilasurendra@gmail.com>
2013-10-26 11:10:58 +0530

    Added class to quickly get config file paths.

---
+++ src/main/java/org/investovator/ui/utils/ConfigHelper.java
@@ -0,0 +1,40 @@
+/*
+ * investovator, Stock Market Gaming framework
+ * Copyright (C) 2013  investovator
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.investovator.ui.utils;
+
+import com.vaadin.server.VaadinService;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class ConfigHelper {
+
+    static String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+
+
+    public static String getDatabaseConfigProperties(){
+        return basepath + "/WEB-INF/configuration/database.properties";
+    }
+
+    public static String getBasepath(){
+        return basepath;
+    }
+
+}
