134152e4d4b8092e1ea23ddcbd05b471e522e931
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-11-14 16:09:13 +0530

    Added ticker data game type

--- src/main/java/org/investovator/ui/dataplayback/gametype/GameTypes.java
+++ src/main/java/org/investovator/ui/dataplayback/gametype/GameTypes.java
@@ -3,6 +3,8 @@ package org.investovator.ui.dataplayback.gametype;
 import org.investovator.core.data.api.utils.TradingDataAttribute;
 import org.investovator.dataplaybackengine.player.type.PlayerTypes;
 
+import java.util.ArrayList;
+
 /**
  * @author: ishan
  * @version: ${Revision}
@@ -11,11 +13,10 @@ public enum GameTypes implements GameConfiguration {
 
     DAILY_SUMMARY_CLOSING_PRICE_GAME{
         @Override
-        public TradingDataAttribute[] getInterestedAttributes() {
-            TradingDataAttribute[] attrs=new TradingDataAttribute[2];
-            attrs[0]=TradingDataAttribute.DAY;
-            attrs[1]=TradingDataAttribute.CLOSING_PRICE;
-
+        public ArrayList<TradingDataAttribute> getInterestedAttributes() {
+            ArrayList<TradingDataAttribute> attrs=new ArrayList<>();
+            attrs.add(TradingDataAttribute.DAY);
+            attrs.add(TradingDataAttribute.CLOSING_PRICE);
             return attrs;
         }
 
@@ -31,6 +32,34 @@ public enum GameTypes implements GameConfiguration {
         @Override
         public String getDescription() {
             return "Daily Summary game based on closing prices of stocks";
+        }
+    },
+
+    TICKER_DATA_GAME{
+        @Override
+        public ArrayList<TradingDataAttribute> getInterestedAttributes() {
+            ArrayList<TradingDataAttribute> attrs=new ArrayList<>();
+            attrs.add(TradingDataAttribute.DAY);
+            attrs.add(TradingDataAttribute.PRICE);
+
+            return attrs;
+        }
+
+        @Override
+        public PlayerTypes getPlayerType() {
+            return PlayerTypes.REAL_TIME_DATA_PLAYER;
+        }
+
+
+
+        @Override
+        public TradingDataAttribute getAttributeToMatch() {
+            return TradingDataAttribute.PRICE;
+        }
+
+        @Override
+        public String getDescription() {
+            return "Ticker data based game traded prices of stocks";
         }
     }
 
