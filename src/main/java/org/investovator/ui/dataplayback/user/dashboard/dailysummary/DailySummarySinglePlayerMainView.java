fdb7b9c85a5c2d9ca3ea58bf8cf3e0532fb8486d
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-11-14 17:40:58 +0530

    Daily summary game single player mode complete. resolves #22 . resolves #33

--- src/main/java/org/investovator/ui/dataplayback/user/dashboard/dailysummary/DailySummarySinglePlayerMainView.java
+++ src/main/java/org/investovator/ui/dataplayback/user/dashboard/dailysummary/DailySummarySinglePlayerMainView.java
@@ -25,6 +25,8 @@ import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanContainer;
 import com.vaadin.ui.*;
 import org.investovator.controller.dataplaybackengine.DataPlaybackGameFacade;
+import org.investovator.core.commons.utils.Portfolio;
+import org.investovator.core.commons.utils.Terms;
 import org.investovator.core.data.api.utils.TradingDataAttribute;
 import org.investovator.dataplaybackengine.events.StockUpdateEvent;
 import org.investovator.dataplaybackengine.exceptions.GameFinishedException;
@@ -34,7 +36,9 @@ import org.investovator.dataplaybackengine.exceptions.UserJoinException;
 import org.investovator.dataplaybackengine.exceptions.player.PlayerStateException;
 import org.investovator.dataplaybackengine.market.OrderType;
 import org.investovator.dataplaybackengine.player.DailySummaryDataPLayer;
+import org.investovator.dataplaybackengine.utils.DateUtils;
 import org.investovator.ui.authentication.Authenticator;
+import org.investovator.ui.dataplayback.beans.PortfolioBean;
 import org.investovator.ui.dataplayback.beans.StockNamePriceBean;
 import org.investovator.ui.dataplayback.util.DataPlaybackEngineStates;
 import org.investovator.ui.utils.dashboard.dataplayback.BasicMainView;
@@ -72,11 +76,13 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
     }
 
 
-    public Chart buildMainChart(){
-
+    @Override
+    public Chart buildMainChart() {
         Chart chart = new Chart();
-//        chart.setHeight("350px");
-//        chart.setWidth("90%");
+        chart.setHeight(70,Unit.MM);
+
+//        chart.setWidth("250px");
+//        chart.setSizeFull();
 
         Tooltip tooltip = new Tooltip();
         tooltip.setShared(true);
@@ -87,33 +93,47 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
 
         Configuration configuration = new Configuration();
         configuration.setTooltip(tooltip);
-        configuration.getChart().setType(ChartType.SPLINE);
+        configuration.getChart().setType(ChartType.LINE);
 
         PlotOptionsLine plotOptions = new PlotOptionsLine();
         plotOptions.setDataLabels(new Labels(true));
         plotOptions.setEnableMouseTracking(false);
+        //performance related
+        plotOptions.setShadow(false);
+
         configuration.setPlotOptions(plotOptions);
 
         configuration.getxAxis().setType(AxisType.DATETIME);
         configuration.getxAxis().setDateTimeLabelFormats(
                 new DateTimeLabelFormats("%e. %b", "%b"));
 
+        configuration.getyAxis().setTitle("Price");
+
         if (DataPlaybackEngineStates.playingSymbols != null) {
             for (String stock : DataPlaybackEngineStates.playingSymbols) {
                 DataSeries ls = new DataSeries();
                 ls.setName(stock);
-//                ls.add(new DataSeriesItem(new Date(),100));
+
+                //add dummy points to fill it up
+                for(int counter=1;counter<=OHLC_CHART_LENGTH;counter++){
+                    ls.add(new DataSeriesItem
+                            (DateUtils.decrementTimeBySeconds((OHLC_CHART_LENGTH - counter),
+                                    DataPlaybackEngineStates.gameStartDate),0));
+                }
+
                 configuration.addSeries(ls);
+
             }
         }
-        chart.drawChart(configuration);
 
+        chart.setImmediate(true);
+        chart.drawChart(configuration);
         //disable trademark
         chart.getConfiguration().disableCredits();
-        chart.getConfiguration().getTitle().setText("Stock Closing Prices");
 
+
+        chart.getConfiguration().setTitle("Real-time Stock Prices");
         return chart;
-
     }
 
     @Override
@@ -130,16 +150,26 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
 //                System.out.println();
 
 //                if (DataPlaybackEngineStates.currentGameMode== PlayerTypes.DAILY_SUMMARY_PLAYER){
-                    try {
-                        Boolean status=player.executeOrder(stocksList.getValue().toString(),
-                                Integer.parseInt(quantity.getValue().toString()), ((OrderType) orderSide.getValue()),
-                                Authenticator.getInstance().getCurrentUser());
-                        Notification.show(status.toString());
-                    } catch (InvalidOrderException e) {
-                        Notification.show(e.getMessage());
-                    } catch (UserJoinException e) {
-                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                try {
+                    Boolean status= player.executeOrder(stocksList.getValue().toString(),
+                            Integer.parseInt(quantity.getValue().toString()), ((OrderType) orderSide.getValue()),
+                            userName);
+                    //if the transaction was a success
+                    if(status){
+                        updatePortfolioTable(stocksList.getValue().toString());
+
+                        //update the pie chart
+                        updatePieChart(stocksList.getValue().toString());
                     }
+                    else{
+
+                        Notification.show(status.toString());
+                    }
+                } catch (InvalidOrderException e) {
+                    Notification.show(e.getMessage());
+                } catch (UserJoinException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
 //                }
 
 //                if (DataPlaybackEngineStates.currentGameMode==PlayerTypes.REAL_TIME_DATA_PLAYER){
@@ -156,7 +186,6 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
 //                }
 
             }});
-
         orderSide.addValueChangeListener(new Property.ValueChangeListener() {
             @Override
             public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
@@ -180,31 +209,37 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
                 try {
                     StockUpdateEvent[] events = player.playNextDay();
                     //iterate every event
-                    for (StockUpdateEvent event : events) {
+                    for (final StockUpdateEvent event : events) {
                         //iterate every series in the chart at the moment
                         for (Series series : mainChart.getConfiguration().getSeries()) {
-                            DataSeries dSeries = (DataSeries) series;
+                            final DataSeries dSeries = (DataSeries) series;
                             //if there's a match
-                            if (event.getStockId().equals(dSeries.getName())) {
-                                if (mainChart.isConnectorEnabled()) {
-                                    getSession().lock();
-                                    try {
+                            if (event.getStockId().equals(dSeries.getName()) && (event.getData()!=null)) {
+                                final float value;
+                                //if new data is available
+                                if(event.getData()!=null ){
+                                    value=event.getData().get(DataPlaybackEngineStates.gameConfig.getAttributeToMatch());
+                                }
+                                else {
+                                    //get the value of the last stock
+                                    value=dSeries.get(dSeries.size()-1).getY().floatValue();
+                                    System.out.println("missing - "+event.getTime()+" - "+value);
+                                }
+
+                                UI.getCurrent().access(new Runnable() {
+                                    @Override
+                                    public void run() {
                                         if (dSeries.getData().size() > OHLC_CHART_LENGTH) {
 
-                                            dSeries.add(new DataSeriesItem(event.getTime(),
-                                                    event.getData().get(TradingDataAttribute.PRICE)), true, true);
+                                            dSeries.add(new DataSeriesItem(event.getTime(),value), true, true);
 
                                         } else {
-                                            dSeries.add(new DataSeriesItem(event.getTime(),
-                                                    event.getData().get(TradingDataAttribute.PRICE)));
+                                            dSeries.add(new DataSeriesItem(event.getTime(),value));
 
                                         }
-                                        mainChart.setImmediate(true);
 
-                                    } finally {
-                                        getSession().unlock();
                                     }
-                                }
+                                });
 
 
                             }
@@ -216,21 +251,26 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
                                 stockPriceTable.getContainerDataSource();
 
 
-                        if (stockPriceTable.isConnectorEnabled()) {
+                        if (stockPriceTable.isConnectorEnabled() && (event.getData()!=null)) {
                             getSession().lock();
                             try {
                                 beans.removeItem(event.getStockId());
                                 beans.addBean(new StockNamePriceBean(event.getStockId(),
-                                        event.getData().get(TradingDataAttribute.PRICE)));
+                                        event.getData().get(TradingDataAttribute.CLOSING_PRICE)));
                             } finally {
                                 getSession().unlock();
                             }
                         }
 
+                        //update the pie chart
+                        updatePieChart(event.getStockId());
+
                     }
 
 
                 } catch (GameFinishedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (UserJoinException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
 
@@ -243,6 +283,59 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
         buttonsBar.addComponent(buySellButton);
 
         return buttonsBar;
+    }
+
+    public void updatePieChart(final String stockId)
+            throws  UserJoinException {
+
+        BeanContainer<String,StockNamePriceBean> beans = (BeanContainer<String,StockNamePriceBean>)
+                stockPriceTable.getContainerDataSource();
+
+        Portfolio portfolio=this.player.getMyPortfolio(this.userName);
+
+        //since we know that there's only one data series
+        final DataSeries dSeries = (DataSeries) stockPieChart.getConfiguration().getSeries().get(0);
+
+
+
+        //if this is an update for a stock that the user has already bought
+        if(portfolio.getShares().containsKey(stockId)){
+            float price =beans.getItem(stockId).getBean().getPrice();
+            double quantity= portfolio.getShares().get(stockId).get(Terms.QNTY);
+
+            //update the chart
+            final DataSeriesItem item=dSeries.get(stockId);
+            item.setY(price*quantity);
+//            dSeries.update(item);
+
+            UI.getCurrent().access(new Runnable() {
+                @Override
+                public void run() {
+                    dSeries.update(item);
+                    stockPieChart.drawChart();
+                    //UI.getCurrent().push();
+                    //System.out.println("pushed");
+                    getUI().push();
+                }
+            });
+
+        }
+
+
+//
+//        if (stockPieChart.isConnectorEnabled()) {
+//            getSession().lock();
+//            try {
+//                dSeries.update(dSeries.get(event.getStockId()));
+//                stockPieChart.setImmediate(true);
+//                stockPieChart.drawChart();
+//
+//            } finally {
+//                getSession().unlock();
+//            }
+//        }
+
+
     }
 
     @Override
@@ -322,5 +415,28 @@ public class DailySummarySinglePlayerMainView extends BasicMainView {
         chart.drawChart(conf);
         chart.setImmediate(true);
         return chart;
+    }
+
+    public void updatePortfolioTable(final String stockID){
+        final BeanContainer<String,PortfolioBean> beans = (BeanContainer<String,PortfolioBean>)
+                portfolioTable.getContainerDataSource();
+
+        UI.getCurrent().access(new Runnable() {
+            @Override
+            public void run() {
+                //if the stock is already bought
+                if(beans.containsId(stockID)){
+                    beans.removeItem(stockID);
+                }
+                try {
+                    double price = player.getMyPortfolio(userName).getShares().get(stockID).get(Terms.PRICE);
+                    int quantity =player.getMyPortfolio(userName).getShares().get(stockID).get(Terms.QNTY).intValue();
+                    beans.addBean(new PortfolioBean(stockID,price, quantity));
+                } catch (UserJoinException e) {
+                    Notification.show("First joint the game", Notification.Type.ERROR_MESSAGE);
+                }
+            }
+        });
+
     }
 }
