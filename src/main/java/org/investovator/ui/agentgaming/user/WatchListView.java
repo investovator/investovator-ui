520e3656d8fe7b9fa5a337e04e8f1fafaaad4764
Amila Surendra <amilasurendra@gmail.com>
2013-11-17 09:15:45 +0530

    added new view to display watchlist.

---
+++ src/main/java/org/investovator/ui/agentgaming/user/WatchListView.java
@@ -0,0 +1,15 @@
+package org.investovator.ui.agentgaming.user;
+
+import org.investovator.ui.utils.dashboard.DashboardPanel;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class WatchListView extends DashboardPanel {
+
+    @Override
+    public void onEnter() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
