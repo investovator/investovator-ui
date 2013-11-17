7b9ec04c3acac41bf1f6e1924bf3b28657c36dee
Amila Surendra <amilasurendra@gmail.com>
2013-11-17 10:29:37 +0530

    Added multi-series chart for analysis.

---
+++ src/main/java/org/investovator/ui/analysis/MultiPlotTimeSeriesChart.java
@@ -0,0 +1,75 @@
+package org.investovator.ui.analysis;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.*;
+import org.investovator.ui.agentgaming.beans.TimeSeriesNode;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class MultiPlotTimeSeriesChart extends Chart {
+    private static final int numberOfPoints = 10;
+    private HashMap<String, DataSeries> series = new HashMap<>();
+
+    public MultiPlotTimeSeriesChart(){
+        setHeight("300px");
+        setWidth("90%");
+        setCaption("Watchlist Summary");
+
+        final Configuration configuration = new Configuration();
+        configuration.setTitle("Last Traded Price");
+
+        configuration.getChart().setType(ChartType.SPLINE);
+        configuration.disableCredits();
+        configuration.getxAxis().setType(AxisType.DATETIME);
+
+        drawChart(configuration);
+    }
+
+
+    public void addSeries(String sereiesName, ArrayList<TimeSeriesNode> data){
+
+        if(series.containsKey(sereiesName)) return;
+
+        final DataSeries dataSeries = new DataSeries();
+
+        //Show last "numberOfPoints" number of data points
+        int startIndex = data.size() <= numberOfPoints ? 0 : ( data.size() - 1) - numberOfPoints;
+
+        for(int count = startIndex; count<data.size(); count++){
+            dataSeries.add(new DataSeriesItem(data.get(count).getDate(), data.get(count).getValue()));
+        }
+
+        dataSeries.setName(sereiesName);
+        series.put(sereiesName,dataSeries);
+
+        getUI().access(new Runnable() {
+            @Override
+            public void run() {
+                getConfiguration().addSeries(dataSeries);
+                getUI().push();
+            }
+        });
+
+    }
+
+    public void insertDataPoint(final String stockID, final Date date, final Number number){
+        getUI().access(new Runnable() {
+            @Override
+            public void run() {
+                if(series.get(stockID).size() < numberOfPoints){
+                    series.get(stockID).add(new DataSeriesItem(date, number));
+                    getUI().push();
+                }else{
+                    series.get(stockID).add(new DataSeriesItem(date, number),true,true);
+                    getUI().push();
+                }
+            }
+        });
+    }
+}
