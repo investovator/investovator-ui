c08cddcc58d5accb5c56623cf597972358b55f5f
Amila Surendra <amilasurendra@gmail.com>
2013-11-18 20:55:55 +0530

    Added more properties.

--- src/main/java/org/investovator/ui/main/beans/CompanyDataBean.java
+++ src/main/java/org/investovator/ui/main/beans/CompanyDataBean.java
@@ -1,13 +1,17 @@
 package org.investovator.ui.main.beans;
 
+import java.util.Date;
+
 /**
  * @author Amila Surendra
  * @version $Revision
  */
-public class CompanyBean {
+public class CompanyDataBean {
 
     private String stockID;
     private String companyName;
+    Date dataStart;
+    Date dataEnd;
 
     public String getStockID() {
         return stockID;
@@ -23,5 +27,21 @@ public class CompanyBean {
 
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
+    }
+
+    public Date getDataStart() {
+        return dataStart;
+    }
+
+    public void setDataStart(Date dataStart) {
+        this.dataStart = dataStart;
+    }
+
+    public Date getDataEnd() {
+        return dataEnd;
+    }
+
+    public void setDataEnd(Date dataEnd) {
+        this.dataEnd = dataEnd;
     }
 }
