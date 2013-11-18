b1508c0182ebd72fa35946e3e4aca80d9b0b3df6
Amila Surendra <amilasurendra@gmail.com>
2013-11-18 20:38:28 +0530

    Created a bean for company table.

---
+++ src/main/java/org/investovator/ui/main/beans/CompanyBean.java
@@ -0,0 +1,27 @@
+package org.investovator.ui.main.beans;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class CompanyBean {
+
+    private String stockID;
+    private String companyName;
+
+    public String getStockID() {
+        return stockID;
+    }
+
+    public void setStockID(String stockID) {
+        this.stockID = stockID;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
