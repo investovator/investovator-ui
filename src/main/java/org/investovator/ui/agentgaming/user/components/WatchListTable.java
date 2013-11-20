a13df6dbed3ec2b0980a0a633cab87d3b2df91b6
Amila Surendra <amilasurendra@gmail.com>
2013-11-20 23:06:13 +0530

    Created watchlist edit table.

---
+++ src/main/java/org/investovator/ui/agentgaming/user/components/WatchListTable.java
@@ -0,0 +1,101 @@
+package org.investovator.ui.agentgaming.user.components;
+
+import com.vaadin.data.Property;
+import com.vaadin.data.util.BeanContainer;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Table;
+import org.investovator.core.data.api.CompanyData;
+import org.investovator.core.data.api.CompanyDataImpl;
+import org.investovator.core.data.api.UserData;
+import org.investovator.core.data.api.UserDataImpl;
+import org.investovator.core.data.exeptions.DataAccessException;
+import org.investovator.ui.authentication.Authenticator;
+import org.investovator.ui.main.beans.CompanyDataBean;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class WatchListTable extends Table implements Button.ClickListener {
+
+
+    public WatchListTable() {
+
+        setWidth("90%");
+        setHeight("300px");
+        setSelectable(true);
+        setImmediate(true);
+
+        setColumns();
+
+    }
+
+    public void updateTable(){
+        removeAllItems();
+        setTableRows();
+    }
+
+    public void addCompany(String stockID, String companyName) {
+        Button removeButton = new Button("Remove");
+        removeButton.setData(stockID);
+        removeButton.addClickListener(this);
+        addItem(new Object[]{stockID, companyName, removeButton}, stockID);
+    }
+
+
+    private void setColumns() {
+
+        addContainerProperty("Symbol", String.class, null);
+        addContainerProperty("Company Name", String.class, null);
+        addContainerProperty("Remove",       Button.class, null);
+
+    }
+
+
+    @Override
+    public void buttonClick(Button.ClickEvent clickEvent) {
+
+        String removedStock = clickEvent.getButton().getData().toString();
+
+        try {
+            UserData userData = new UserDataImpl();
+            userData.deleteFromWatchList(Authenticator.getInstance().getCurrentUser(), removedStock);
+            updateTable();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public void setTableRows(){
+
+
+        try {
+            UserData userData = new UserDataImpl();
+            CompanyData companyData =new CompanyDataImpl();
+            HashMap<String,String> companyNames = companyData.getCompanyIDsNames();
+
+            ArrayList<String> watchlist =  userData.getWatchList(Authenticator.getInstance().getCurrentUser());
+
+            if(watchlist != null){
+                for(String stock : watchlist){
+                    addCompany(stock, companyNames.get(stock));
+                }
+            }
+
+
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
