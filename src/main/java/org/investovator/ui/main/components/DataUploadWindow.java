467c18998a592f8e1d25cf57601ab18b26589746
Amila Surendra <amilasurendra@gmail.com>
2013-11-20 10:31:35 +0530

    Data file uploading completed.

--- src/main/java/org/investovator/ui/main/components/DataUploadWindow.java
+++ src/main/java/org/investovator/ui/main/components/DataUploadWindow.java
@@ -22,32 +22,49 @@ import com.vaadin.data.Property;
 import com.vaadin.ui.*;
 import org.investovator.core.data.api.CompanyData;
 import org.investovator.core.data.api.CompanyDataImpl;
+import org.investovator.core.data.api.CompanyStockTransactionsData;
+import org.investovator.core.data.api.CompanyStockTransactionsDataImpl;
 import org.investovator.core.data.exeptions.DataAccessException;
+import org.investovator.ui.utils.ConfigHelper;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
 /**
  * @author Amila Surendra
  * @version $Revision
  */
-public class DataUploadWindow extends Window{
+public class DataUploadWindow extends Window implements Upload.SucceededListener{
 
     //Layout
     private VerticalLayout content;
-    private ComboBox companySelect;
     private HorizontalLayout companySelectLayout;
     private HorizontalLayout companyDetailUploadLayout;
+    private FormLayout newCompanyForm;
+    private ComboBox companySelect;
+    private ComboBox dataTypeSelect;
     private Upload dataUpload;
     private Upload detailUpload;
     private CheckBox newCompanyCheckBox;
+    private Button submitButton;
+
+    private TextField symbol;
+    private TextField companyName;
 
     //state
-    boolean newCompanySettingsShown;
+    boolean newCompany;
     private String selectedStock;
+    private CompanyStockTransactionsData.DataType selectedDataType;
+
+    private String OHCL_DATA = "Summary Data";
+    private String TICKER_DATA = "Ticker Data";
+
 
     public DataUploadWindow(){
 
-        newCompanySettingsShown = false;
+        newCompany = false;
         setLayout();
 
     }
@@ -58,12 +75,15 @@ public class DataUploadWindow extends Window{
         setCaption("Upload Data");
         setResizable(false);
         setClosable(true);
+        setModal(true);
         center();
 
         content = new VerticalLayout();
         content.setSpacing(true);
+
         companySelectLayout = new HorizontalLayout();
         companyDetailUploadLayout = new HorizontalLayout();
+        newCompanyForm = new FormLayout();
 
         newCompanyCheckBox = new CheckBox("New Company");
 
@@ -73,6 +93,9 @@ public class DataUploadWindow extends Window{
         companySelect.setNullSelectionAllowed(false);
         companySelect.setImmediate(true);
 
+        dataTypeSelect = new ComboBox();
+        dataTypeSelect.setNullSelectionAllowed(false);
+
         dataUpload = new Upload();
         dataUpload.setCaption("Select Data File");
         dataUpload.setButtonCaption(null);
@@ -81,13 +104,25 @@ public class DataUploadWindow extends Window{
         detailUpload.setCaption("select Company Info XML");
         detailUpload.setButtonCaption(null);
 
+        symbol = new TextField("Company Symbol");
+        symbol.setRequired(true);
+        companyName = new TextField("Company Name");
+        companyName.setRequired(true);
+
+        submitButton = new Button("Upload Data");
+
+        newCompanyForm.addComponent(symbol);
+        newCompanyForm.addComponent(companyName);
+
         companyDetailUploadLayout.addComponent(detailUpload);
 
         companySelectLayout.addComponent(companySelect);
         companySelectLayout.addComponent(newCompanyCheckBox);
 
         content.addComponent(companySelectLayout);
+        content.addComponent(dataTypeSelect);
         content.addComponent(dataUpload);
+        content.addComponent(submitButton);
 
         setContent(content);
         setDefaults();
@@ -104,15 +139,33 @@ public class DataUploadWindow extends Window{
             for(String stock : companyData.getAvailableStockIds()){
                   companySelect.addItem(stock);
             }
+            companySelect.select(companyData.getAvailableStockIds().get(0));
+            selectedStock=companyData.getAvailableStockIds().get(0);
 
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
 
+        dataTypeSelect.addItem(OHCL_DATA);
+        dataTypeSelect.addItem(TICKER_DATA);
+        dataTypeSelect.select(OHCL_DATA);
+        selectedDataType = CompanyStockTransactionsData.DataType.OHLC;
+
 
     }
 
     private void bindListeners(){
+
+        submitButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+
+                if(newCompany) selectedStock = symbol.getValue();
+
+                dataUpload.submitUpload();
+
+            }
+        });
 
 
         newCompanyCheckBox.addValueChangeListener(new Property.ValueChangeListener() {
@@ -123,9 +176,27 @@ public class DataUploadWindow extends Window{
             }
         });
 
+        companySelect.addValueChangeListener(new Property.ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
+                selectedStock = (String) companySelect.getValue();
+            }
+        });
+
+        dataTypeSelect.addValueChangeListener(new Property.ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
+                String valueString = (String) dataTypeSelect.getValue();
+                if(valueString.equals(TICKER_DATA)) selectedDataType = CompanyStockTransactionsData.DataType.TICKER;
+                if(valueString.equals(OHCL_DATA)) selectedDataType = CompanyStockTransactionsData.DataType.OHLC;
+
+            }
+        });
+
 
         //File Receivers
         dataUpload.setReceiver(new DataFileReceiver());
+        dataUpload.addSucceededListener(this);
         detailUpload.setReceiver(new DetailFileReceiver());
 
     }
@@ -133,29 +204,72 @@ public class DataUploadWindow extends Window{
 
     private void setNewCompanySettings(boolean enable){
 
-        if(enable && !newCompanySettingsShown){
+        if(enable && !newCompany){
 
-            content.addComponent(companyDetailUploadLayout, 1);
+            content.addComponent(newCompanyForm, 1);
             companySelect.setEnabled(false);
-            newCompanySettingsShown = true;
+            newCompany = true;
 
-        }else if(!enable && newCompanySettingsShown){
+        }else if(!enable && newCompany){
 
-            content.removeComponent(companyDetailUploadLayout);
+            content.removeComponent(newCompanyForm);
             companySelect.setEnabled(true);
-            newCompanySettingsShown = false;
+            newCompany = false;
 
         }
 
 
     }
 
+    @Override
+    public void uploadSucceeded(Upload.SucceededEvent succeededEvent) {
+
+        if(succeededEvent.getUpload().getReceiver()instanceof DataFileReceiver){
+
+            if(newCompany){
+
+                try {
+                    new CompanyDataImpl().addCompanyData(selectedStock, companyName.getValue(), 100000);
+                } catch (DataAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            CompanyStockTransactionsData historyData = new CompanyStockTransactionsDataImpl();
+            try {
+                historyData.importCSV(selectedDataType,selectedStock,"MM/dd/yyyy",new File(((DataFileReceiver)succeededEvent.getUpload().getReceiver()).getFile().getAbsolutePath()));
+            } catch (DataAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     private class DataFileReceiver implements Upload.Receiver{
 
+        private File file;
+
+        public File getFile(){
+            return  file;
+        }
+
         @Override
-        public OutputStream receiveUpload(String s, String s2) {
-            return null;
+        public OutputStream receiveUpload(String fileName, String mimeType) {
+            // Create upload stream
+            FileOutputStream fos = null; // Stream to write to
+            try {
+                // Open the file for writing.
+                file = new File(ConfigHelper.getUploadPath() + selectedStock+".csv");
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                fos = new FileOutputStream(file);
+            } catch (final java.io.FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return fos; // Return the output stream to write to
         }
     }
 
